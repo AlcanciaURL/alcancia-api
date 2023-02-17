68b754cf1ff351275a0fe80907e02f97ffc495bd
axelgolin4 <axelgolin4@gmail.com>
2023-02-16 20:31:59 -0600

    Basic endpoint configuration

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { User as UserModel } from '@prisma/client';
+import { UserService } from './user.service';
+
+
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getAllUsers(): Promise<UserModel[]>{
+    return this.userService.users({});
+  }
+
+  @Post()
+  async signupUser(
+    @Body() userData: { name?: string; email: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+
+}
