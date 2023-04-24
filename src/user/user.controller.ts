02e0399a061a29baefea8b49fc814015f1e3f723
axelgolin4 <axelgolin4@gmail.com>
2023-04-23 22:47:08 -0600

    Swagger configuration

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -11,16 +11,26 @@ import { User } from '@prisma/client';
 import { UserDTO } from './dtos/user.dto';
 import { UserUpdateDTO } from './dtos/user.update.dto';
 import { UserService } from './user.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Usuarios')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiOperation({
+    summary: 'listado de usuarios',
+    description: 'En este endpoint encontrara a todos los usuarios activos',
+  })
   @Get('/')
   async getAllUsers(): Promise<User[]> {
     return this.userService.getAll({});
   }
 
+  @ApiOperation({
+    summary: 'Usuario por ID',
+    description: 'En este endpoint encontrara a un usuario buscado por ID',
+  })
   @Get(':id')
   async getOneUser(@Param('id') idUser: User['idUser']): Promise<User> {
     return this.userService.getOne({
@@ -28,11 +38,19 @@ export class UserController {
     });
   }
 
+  @ApiOperation({
+    summary: 'Crear un nuevo Usuario',
+    description: 'En este endpoint se podra crear un nuevo usuario',
+  })
   @Post('/')
   async signupUser(@Body() userData: UserDTO): Promise<User> {
     return this.userService.create(userData);
   }
 
+  @ApiOperation({
+    summary: 'Modificar Usuario',
+    description: 'En este endpoint se podra modificar un usuario',
+  })
   @Put(':id')
   updateUser(@Param('id') idUser: User['idUser'], @Body() data: UserUpdateDTO) {
     return this.userService.update(
@@ -43,6 +61,10 @@ export class UserController {
     );
   }
 
+  @ApiOperation({
+    summary: 'Eliminar Usuario',
+    description: 'En este endpoint se podra eliminar un usuario',
+  })
   @Delete(':id')
   deleteUser(@Param('id') idUser: User['idUser']) {
     return this.userService.delete({ idUser });
