2f90e6e38ae022d1ec534f8be78d88fe50816194
VinRicci <vinicioricci@gmail.com>
2023-04-24 16:26:18 -0600

    Documentation for workspace and invitation

--- src/invitation/invitation.controller.ts
+++ src/invitation/invitation.controller.ts
@@ -7,18 +7,30 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Invitation } from '@prisma/client';
 import { InvitationService } from './invitation.service';
 
+@ApiTags('Invitaciones')
 @Controller('invitation')
 export class InvitationController {
   constructor(private readonly invitationService: InvitationService) {}
 
+  @ApiOperation({
+    summary: 'Listado de invitaciones',
+    description:
+      'En este endpoint encontraran todas las invitaciones existentes',
+  })
   @Get('/')
   async getAll(): Promise<Invitation[]> {
     return this.invitationService.getAll();
   }
 
+  @ApiOperation({
+    summary: 'Busqueda de invitaciones por Id',
+    description:
+      'En este endpoint encontrara la invitacion buscada por un id especifico',
+  })
   @Get(':id')
   async getOne(
     @Param('id') idInvitation: Invitation['idInvitation'],
@@ -28,11 +40,20 @@ export class InvitationController {
     });
   }
 
+  @ApiOperation({
+    summary: 'Crear invitaciones',
+    description: 'En este endpoint se crea la invitacion',
+  })
   @Post('/')
   async create(@Body() invitation: Invitation): Promise<Invitation> {
     return this.invitationService.create(invitation);
   }
 
+  @ApiOperation({
+    summary: 'Actualiza invitaciones',
+    description:
+      'En este endpoint se pueden realizar modificaciones de las invitaciones',
+  })
   @Put(':id')
   update(
     @Param('id') idInvitation: Invitation['idInvitation'],
@@ -46,6 +67,10 @@ export class InvitationController {
     );
   }
 
+  @ApiOperation({
+    summary: 'Eliminar invitaciones',
+    description: 'En este endpoint se eliminan las invitaciones',
+  })
   @Delete(':id')
   delete(@Param('id') idInvitation: Invitation['idInvitation']) {
     return this.invitationService.delete({ idInvitation });
--- src/workspace/workspace.controller.ts
+++ src/workspace/workspace.controller.ts
@@ -7,18 +7,30 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Workspace } from '@prisma/client';
 import { WorkspaceService } from './workspace.service';
 
+@ApiTags('Espacio de trabajo')
 @Controller('workspace')
 export class WorkspaceController {
   constructor(private readonly workspaceService: WorkspaceService) {}
 
+  @ApiOperation({
+    summary: 'Listado de los espacios de trabajo',
+    description:
+      'En este endpoint encontraran todos los espacios de trabajo creados',
+  })
   @Get('/')
   async getAll(): Promise<Workspace[]> {
     return this.workspaceService.getAll();
   }
 
+  @ApiOperation({
+    summary: 'Espacios de trabajo buscados por id',
+    description:
+      'En este endpoint encontrara los espacios de trabajo cuando necesite buscar por id',
+  })
   @Get(':id')
   async getOne(
     @Param('id') idWorkspace: Workspace['idWorkspace'],
@@ -28,11 +40,20 @@ export class WorkspaceController {
     });
   }
 
+  @ApiOperation({
+    summary: 'Crear espacio de trabajo',
+    description: 'En este endpoint ese crean los espacios de trabajo',
+  })
   @Post('/')
   async create(@Body() work: Workspace): Promise<Workspace> {
     return this.workspaceService.create(work);
   }
 
+  @ApiOperation({
+    summary: 'Actualizar la informacion de los espacios de trabajo',
+    description:
+      'En este endpoint podra actualizar la informacion de los espacios de trabajo en caso de que hayan habido modificaciones',
+  })
   @Put(':id')
   update(
     @Param('id') idWorkspace: Workspace['idWorkspace'],
@@ -46,6 +67,11 @@ export class WorkspaceController {
     );
   }
 
+  @ApiOperation({
+    summary: 'Eliminar un espacios de trabajo',
+    description:
+      'En este endpoint podra eliminar los espacios de trabajo por un id en especifico',
+  })
   @Delete(':id')
   delete(@Param('id') idWorkspace: Workspace['idWorkspace']) {
     return this.workspaceService.delete({ idWorkspace });
