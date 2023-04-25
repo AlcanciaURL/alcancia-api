e5ad30e49f0eda2c3a06796b010f076e6b0cfbf6
axelgolin4 <axelgolin4@gmail.com>
2023-04-25 08:56:16 -0600

    documentacion hecha

--- src/objetive/objetive.controller.ts
+++ src/objetive/objetive.controller.ts
@@ -9,16 +9,26 @@ import {
 } from '@nestjs/common';
 import { Objetive } from '@prisma/client';
 import { ObjetiveService } from './objetive.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Objetivos')
 @Controller('objetive')
 export class ObjetiveController {
   constructor(private readonly objetiveService: ObjetiveService) {}
 
+  @ApiOperation({
+    summary: 'listado de Objetivos',
+    description: 'En este endpoint encontrara a todos los objetivos activos',
+  })
   @Get('/')
   async getAll(): Promise<Objetive[]> {
     return this.objetiveService.getAll();
   }
 
+  @ApiOperation({
+    summary: 'Objetivos por ID',
+    description: 'En este endpoint encontrara a un objetivo buscado por ID',
+  })
   @Get(':id')
   async getOne(
     @Param('id') idObjetive: Objetive['idObjetive'],
@@ -28,11 +38,19 @@ export class ObjetiveController {
     });
   }
 
+  @ApiOperation({
+    summary: 'Crear un nuevo Objetivo',
+    description: 'En este endpoint se podra crear un nuevo objetivo',
+  })
   @Post('/')
   async create(@Body() objetive: Objetive): Promise<Objetive> {
     return this.objetiveService.create(objetive);
   }
 
+  @ApiOperation({
+    summary: 'Modificar Objetivo',
+    description: 'En este endpoint se podra modificar un objetivo',
+  })
   @Put(':id')
   update(
     @Param('id') idObjetive: Objetive['idObjetive'],
@@ -46,6 +64,10 @@ export class ObjetiveController {
     );
   }
 
+  @ApiOperation({
+    summary: 'Eliminar Objetivo',
+    description: 'En este endpoint se podra eliminar un objetivo',
+  })
   @Delete(':id')
   delete(@Param('id') idObjetive: Objetive['idObjetive']) {
     return this.objetiveService.delete({ idObjetive });
--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  const mockUserService = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-    }).compile();
+      providers: [UserService],
+    })
+      .overrideProvider(UserService)
+      .useValue(mockUserService)
+      .compile();
 
     controller = module.get<UserController>(UserController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return a user', () => {
+    expect(controller.getAllUsers());
+  });
+
+  it('should create a user', () => {
+    expect(
+      controller.signupUser({
+        first_name: 'Axel',
+        last_name: 'Rodas',
+        email: 'axelgolin@hotmail.es',
+      }),
+    ).toEqual({
+      first_name: expect.any(String),
+      last_name: expect.any(String),
+      email: expect.any(String),
+    });
   });
 });
